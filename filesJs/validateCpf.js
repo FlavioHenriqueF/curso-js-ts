a0ebe9f3bb5726f6fc93a0dd7ff75b3fe69ec158
Flavio Henrique Franco <flavio3224henrique@gmail.com>
2023-01-12 10:31:03 -0300

    Validando CPF - Correção

--- filesJs/validateCpf.js
+++ filesJs/validateCpf.js
@@ -10,18 +10,19 @@ function ValidaCPF(cpfEnviado) {
 ValidaCPF.prototype.valida = function () {
   if (typeof this.cpfLimpo === "undefined") return false;
   if (this.cpfLimpo.length !== 11) return false;
+  if (this.isSequencia()) return false;
 
   const cpfParcial = this.cpfLimpo.slice(0, -2);
-  const digito1 = this.criarDigito(cpfParcial, 10);
-  const digito2 = this.criarDigito(cpfParcial + digito1, 11);
+  const digito1 = this.criarDigito(cpfParcial);
+  const digito2 = this.criarDigito(cpfParcial + digito1);
 
   const novoCpf = cpfParcial + digito1 + digito2;
   return novoCpf === this.cpfLimpo;
 };
 
-ValidaCPF.prototype.criarDigito = function (numbers, loops) {
-  let i = loops;
+ValidaCPF.prototype.criarDigito = function (numbers) {
   let loop = Array.from(numbers);
+  let i = loop.length + 1;
   let total = [];
 
   loop.forEach((value) => {
@@ -37,6 +38,10 @@ ValidaCPF.prototype.calcularDigit = function (value) {
   const calc = value.reduce((ac, value) => (ac += Number(value)), 0);
   const digito = 11 - (calc % 11);
   return digito > 9 ? "0" : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function () {
+  return this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo;
 };
 
 const cpf = new ValidaCPF("705.484.450-52");
